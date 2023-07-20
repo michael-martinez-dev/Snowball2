b09299fd60928354af42fbb74b219dd7ed16dc6a
Michael Marinez <80479622+MixedMachine@users.noreply.github.com>
2023-07-20 14:13:04 -0600

    Added export button

--- frontend/src/components/DebtExport.vue
+++ frontend/src/components/DebtExport.vue
@@ -1,37 +1,42 @@
 <template>
-    <v-btn
-        flat
-        color="primary"
-        @click="exportDebts"
-    >Export Debts</v-btn>
+<v-btn flat color="primary" @click="exportDebts">Export Debts</v-btn>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import { Bill } from "../models/Bills";
+import {
+    defineComponent
+} from "vue";
+import {
+    Bill
+} from "../models/Bills";
 
 export default defineComponent({
     name: "DebtsTable",
     props: {
         bills: {
-            type: Array as () => Bill[],
+            type: Array as() => Bill[],
             required: true,
         },
     },
     methods: {
         exportDebts() {
             console.log("exportDebts");
-            const csv = this.bills.map((bill) => {
+            let csv = ["Name", "Type", "Total", "Interest"].join(",") + "\n";
+
+            csv += this.bills.map((bill: Bill) => {
                 return [
                     bill.name,
                     bill.type,
-                    bill.total,
-                    bill.interest,
-                    bill.monthlyMin,
-                    bill.monthlyActual,
+                    bill.total > 0? "\"" + bill.total.toLocaleString("en-US", {
+                        style: "currency",
+                        currency: "USD",
+                    }) + "\"" : "$0.00",
+                    bill.interest ? bill.interest.toString() + "%" : "N/A",
                 ].join(",");
             }).join("\n");
-            const blob = new Blob([csv], { type: "text/csv" });
+            const blob = new Blob([csv], {
+                type: "text/csv"
+            });
             const url = window.URL.createObjectURL(blob);
             const a = document.createElement("a");
             a.setAttribute("hidden", "");
